a75301292429fd6366e4be05a4dca5d6eb98500e
moe9195 <moerahmeh95@hotmail.com>
2020-02-23 16:13:03 +0200

    ..

--- task_1/urls.py
+++ task_1/urls.py
@@ -24,13 +24,14 @@ from flights import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('flights/', views.FlightsList.as_view(), name="flights-list"),
-    
-    path('bookings/', views.BookingsList.as_view(), name="bookings-list"), 
+
+    path('bookings/', views.BookingsList.as_view(), name="bookings-list"),
     path('booking/<int:booking_id>/', views.BookingDetails.as_view(), name="booking-details"),
     path('booking/<int:booking_id>/update/', views.UpdateBooking.as_view(), name="update-booking"),
     path('booking/<int:booking_id>/cancel/', views.CancelBooking.as_view(), name="cancel-booking"),
     path('book/<int:flight_id>/', views.BookFlight.as_view(), name="book-flight"),
 
     path('login/', TokenObtainPairView.as_view(), name="login"),
-    path('token/refresh/', TokenRefreshView.as_view(), name="token-refresh")
+    path('token/refresh/', TokenRefreshView.as_view(), name="token-refresh"),
+    path('register/', views.UserCreateAPIView.as_view(), name='register')
 ]
